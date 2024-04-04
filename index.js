9266a874409d6a156baef587e5acbdfcf87e696c
Pedro Previatti <pvlpreviatti@gmail.com>
2024-04-04 16:22:09 -0300

    fixing broken code

--- index.js
+++ index.js
@@ -34,25 +34,27 @@ const url = require('url');
  * SERVER
  */
 
-const replaceTemplate =(template, product) => {
-    let output = template.replace(/{$PRODUCTNAME$}/g, product.productName);
-    output = output.replace(/{$IMAGE$}/g, product.image);
-    output = output.replace(/{$PRICE$}/g, product.price);
-    output = output.replace(/{$FROM$}/g, product.from);
-    output = output.replace(/{$NUTRIENTS$}/g, product.nutrients);
-    output = output.replace(/{$QUANTITY$}/g, product.quantity);
-    output = output.replace(/{$DESCRIPTION$}/g, product.description);
-    output = output.replace(/{$ID$$}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{$NOT_ORGANIC$}/g, 'not_organic');
-}
-
 const overviewTemplate = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const cardTemplate = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const productTemplate = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
+    .replace(/{%IMAGE%}/g, product.image)
+    .replace(/{%PRICE%}/g, product.price)
+    .replace(/{%FROM%}/g, product.from)
+    .replace(/{%NUTRIENTS%}/g, product.nutrients)
+    .replace(/{%QUANTITY%}/g, product.quantity)
+    .replace(/{%DESCRIPTION%}/g, product.description)
+    .replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    
+    return output;
+}
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
@@ -61,8 +63,12 @@ const server = http.createServer((req, res) => {
     if(pathName === '/' || pathName === '/overview') {
         res.writeHead(200, {'Content-type': 'text/html'});
 
-        const cardsHtml = dataObj.map(el => replaceTemplate(cardTemplate, el)).join('');
-        const output = overviewTemplate.replace('{$PRODUCT_CARDS$}', cardsHtml);
+        const cardsHtml = dataObj
+        .map((el) => replaceTemplate(cardTemplate, el))
+        .join('');
+        const output = overviewTemplate
+        .replace('{%PRODUCT_CARDS%}', cardsHtml)
+        .trim();
 
         res.end(output);
     
